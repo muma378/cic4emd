d7d95ae7fd7afc2f5321c1d64f610cedf0dabefe
xiaoyang <muma.378@163.com>
2016-05-09 00:38:23 +0800

    [FIX] fix comment

--- utils/validators.py
+++ utils/validators.py
@@ -7,7 +7,7 @@ from django.utils import timezone
 
 def validate_not_past(value):
     tz_now = timezone.make_aware(datetime.now(), timezone.get_default_timezone())
-    if tz_now - value > timedelta(seconds=300):    # if the error is less than 5 minute
+    if tz_now - value > timedelta(seconds=300):    # if the delayls is less than 5 minute
         raise ValidationError(
             ugettext_lazy(u'发布日期不能早于当前'),
             params={'value':value})
