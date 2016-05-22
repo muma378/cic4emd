9b76012b2218fd32823d41fe4fd94953af4803c0
xiaoyang <muma.378@163.com>
2016-05-22 22:09:19 +0800

    [FIX] css for news list

--- pages/static/js/pages.js
+++ pages/static/js/pages.js
@@ -1,15 +1,26 @@
 $(document).ready(function() {
 	
+	var activate = function (elem) {
+		elem.hover(
+				function (){
+				$(this).siblings(".active").removeClass("active")
+				$( this ).addClass("active")
+			}, function () {
+				$( this ).removeClass("active")
+			}
+		)
+	};
+
 	// add class active when hovering navigation bar
-	$(".cic-navbar-item").hover(
-		function (){
-			$( this ).addClass("active")
-		}, function () {
-			$( this ).removeClass("active")
-		}
-	);
-
-
+	activate($(".cic-navbar-item"));
+	// add class active when hovering news 
+	// activate($(".cic-trend-item"));
+	$(".cic-trend-item").hover(
+				function (){
+				$(this).siblings(".active").removeClass("active")
+				$( this ).addClass("active")
+			}, function () {}
+		)
 
 	
 });
